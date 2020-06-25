438613e16e7a627c2465cd57fdb632adc9a868cd
Armin Ghobadi <armin.ghobadi95@gmail.com>
2020-06-25 19:26:12 -0400

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,35 +1,6 @@
-const path = require("path");
-
-module.exports = function(_env, argv) {
-  const isProduction = argv.mode === "production";
-  const isDevelopment = !isProduction;
-
+  module.exports = api => {
+  api.cache(false)
   return {
-    devtool: isDevelopment && "cheap-module-source-map",
-    entry: "./src/index.js",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "assets/js/[name].[contenthash:8].js",
-      publicPath: "/"
-    },
-    module: {
-      rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          use: {
-            loader: "babel-loader",
-            options: {
-              cacheDirectory: true,
-              cacheCompression: false,
-              envName: isProduction ? "production" : "development"
-            }
-          }
-        }
-      ]
-    },
-    resolve: {
-      extensions: [".js", ".jsx"]
-    }
-  };
-};
+    plugins: ['@babel/plugin-transform-arrow-functions']
+  }
+}
